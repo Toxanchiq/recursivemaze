a2406abecff56b8d3a18b86e948b0d1b0749addd
Toxanchiq <125993244+Toxanchiq@users.noreply.github.com>
2025-04-14 19:49:24 +0600

    Carving added to maze.py
    I have added function to carve a passages

--- maze.py
+++ maze.py
@@ -7,3 +7,20 @@ def generate_maze(rows, cols):
  
     #making maze with walls
     maze = [['#' for _ in range(cols)] for _ in range(rows)]
+ def carve_passages(r, c):
+        #define the moves (jumping two cells) for up, left, right, down
+        directions = [(0, -2), (-2, 0), (0, 2), (2, 0)]
+        random.shuffle(directions)
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            #make sure we stay within the maze and the neighbor is a wall
+            if 0 < nr < rows and 0 < nc < cols and maze[nr][nc] == '#':
+                #make the passage between the two cells
+                maze[r + dr // 2][c + dc // 2] = ' '
+                maze[nr][nc] = ' '
+                carve_passages(nr, nc)
+                
+    #start to carving passages from (1,1)
+    maze[1][1] = ' '
+    carve_passages(1, 1)
+    
